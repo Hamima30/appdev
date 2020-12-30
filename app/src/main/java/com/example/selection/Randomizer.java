74d4706c471945ff9d48a725f3bee96ad41cb25a
Hamima Dalano <mimadalano30@gmail.com>
2020-12-30 19:50:04 +0800

    budget buddy unfinished

--- app/src/main/java/com/example/selection/Randomizer.java
+++ app/src/main/java/com/example/selection/Randomizer.java
@@ -1,7 +1,5 @@
 package com.example.selection;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.InputFilter;
@@ -9,7 +7,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
